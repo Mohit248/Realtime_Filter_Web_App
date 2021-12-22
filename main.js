173acb0c13996e1b470fc4a4f0cf687962569d66
Mohit248 <83233932+Mohit248@users.noreply.github.com>
2021-12-22 20:37:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_Snapshot(){
+    save('my_Filter_Image.png');
+}
