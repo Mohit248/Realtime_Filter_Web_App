22d7996d0203e570dbbbde930852f4aa1df5ae8d
Mohit248 <83233932+Mohit248@users.noreply.github.com>
2022-01-12 20:33:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseX = 0
+noseY = 0
 
+function preload(){
+clown_nose = loadImage("https://i.postimg.cc/JnLVKfcH/580b57fbd9996e24bc43bed5.png");
 }
 
 function setup(){
@@ -14,6 +17,7 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(clown_nose,noseX,noseY,30,30)
 }
 
 function take_Snapshot(){
@@ -27,7 +31,9 @@ function modelLoaded(){
 function gotPoses(results){
   if (results.length > 0) {
       console.log(results);
-      console.log("nose x ="+results[0].pose.nose.x);
-      console.log("nose y ="+results[0].pose.nose.y);
+      noseX = results[0].pose.nose.x-15;
+      noseY = results[0].pose.nose.y-15;
+      console.log("nose x ="+noseX);
+      console.log("nose y ="+noseY);
   }
 }
